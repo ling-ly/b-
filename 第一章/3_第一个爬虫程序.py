fb3059e24c7cefc71d58f641b24d5db3a8fc9d9d
ling-ly <75435135+ling-ly@users.noreply.github.com>
2023-02-18 14:29:50 +0800

    Add files via upload

---
+++ 第一章/3_第一个爬虫程序.py
@@ -0,0 +1,19 @@
+# 作者：凌建宝
+# 开发时间：2023/2/18 9:33
+# 爬虫：通过编写程序来获取到互联网上的资源
+# b站？百度
+# 需求：用程序模拟浏览器，输入一个网址，从该网站中获取到资源或者内容
+# python搞定以上需求，特别简单
+
+from urllib.request import urlopen
+# 从urllib库中引入urlopen,即请求打开链接
+url = "http://www.baidu.com/"
+resp = urlopen(url)     # 打开百度并将网页源代码赋予resp
+
+# print(resp.read().decode("utf-8"))
+# # resp.read()得到的是二进制代码，所有要使用decode方法进行解码
+with open("mybaidu.html","w",encoding="utf-8") as f:    # 创建文件
+    f.write((resp.read().decode("utf-8")))    # 保存写入  # 读取网页源代码
+# 如果发现读的文件很小是window打开文件默认是以“gbk“编码的，可能造成不识别unicode字符
+# 解决办法"mybaidu.html","w",???encoding="utf-8"???  即？？？这句话？？？
+print("over")
